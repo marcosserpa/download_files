5cf868cf34d81fe3b5f2d40eff9a96b456ab848d
Marcos Serpa <marcosserpa@gmail.com>
2014-07-07 00:12:34 -0300

    Does the download of words with a, mount the URI and saves into source.txt. Does the download of all URIs saved.

---
+++ download-files.rb
@@ -0,0 +1,153 @@
+#!/usr/bin/env ruby
+
+# Ruby script to download .wav files from individual URLs via HTTP/HTTPS/FTP specified in an external file.
+# Author: Marcos Serpa (some source from Tobias Preuss)
+# License: Creative Commons Attribution-ShareAlike 3.0 Unported
+
+require 'net/http'
+require 'net/ftp'
+require 'uri'
+require 'date'
+
+
+# TODO loop for get all letters
+# Save the sources of the .wav files into sources.txt
+def save_sources
+  url = URI.parse("http://www.elearnspanishlanguage.com/pronunciation/audiodictionary-a.html")
+
+  # Saves the HTML content
+  response = Net::HTTP.start(url.host, url.port) { |http| http.get('/pronunciation/audiodictionary-a.html') }
+
+  open("sources.txt", "w") { |file|
+    response.body.lines.each do |line|
+      word = ""
+      word = /(?<=\/sounds\/)\w+(?=.wav)/.match(line).to_s
+
+      if word.eql?("") == false
+        resp = "http://www.elearnspanishlanguage.com/sounds/#{word}.wav #{word}.wav\n"
+        file << resp
+      end
+    end
+  }
+
+  puts "All sources writed in sources.txt!!"
+end
+
+def create_directory(dirname)
+  unless Dir.exists?(dirname)
+    Dir.mkdir(dirname)
+  else
+    puts "Skipping creating directory " + dirname + ". It already exists."
+  end
+end
+
+def read_uris_from_file(file)
+  uris = Array.new
+  File.open(file).each do |line|
+    line = line.strip
+    next if line == nil || line.length == 0
+    parts = line.split(' ')
+    pair = Hash[ [:resource, :filename].zip(parts) ]
+    uris.push(pair)
+  end
+  uris
+end
+
+def download_resource(resource, filename)
+  uri = URI.parse(resource)
+  case uri.scheme.downcase
+  when /http|https/
+    http_download_uri(uri, filename)
+  when /ftp/
+    ftp_download_uri(uri, filename)
+  else
+    puts "Unsupported URI scheme for resource " + resource + "."
+  end
+end
+
+def http_download_uri(uri, filename)
+  puts "Starting HTTP download for: " + uri.to_s
+  http_object = Net::HTTP.new(uri.host, uri.port)
+  http_object.use_ssl = true if uri.scheme == 'https'
+  begin
+    http_object.start do |http|
+      request = Net::HTTP::Get.new uri.request_uri
+      http.read_timeout = 500
+      http.request request do |response|
+        open filename, 'w' do |io|
+          response.read_body do |chunk|
+            io.write chunk
+          end
+        end
+      end
+    end
+  rescue Exception => e
+    puts "=> Exception: '#{e}'. Skipping download."
+    return
+  end
+  puts "Stored download as " + filename + "."
+end
+
+def ftp_download_uri(uri, filename)
+  puts "Starting FTP download for: " + uri.to_s + "."
+  dirname = File.dirname(uri.path)
+  basename = File.basename(uri.path)
+  begin
+    Net::FTP.open(uri.host) do |ftp|
+      ftp.login
+      ftp.chdir(dirname)
+      ftp.getbinaryfile(basename)
+    end
+  rescue Exception => e
+    puts "=> Exception: '#{e}'. Skipping download."
+    return
+  end
+  puts "Stored download as " + filename + "."
+end
+
+def download_resources(pairs)
+  pairs.each do |pair|
+    filename = pair[:filename].to_s
+    resource = pair[:resource].to_s
+    unless File.exists?(filename)
+      download_resource(resource, filename)
+    else
+      puts "Skipping download for " + filename + ". It already exists."
+    end
+  end
+end
+
+
+def main
+  save_sources
+
+  sources_file = ARGV.first
+  uris = read_uris_from_file(sources_file)
+
+  target_dir_name = Date.today.strftime('%y%m%d')
+  create_directory(target_dir_name)
+  Dir.chdir(target_dir_name)
+  puts "Changed directory: " + Dir.pwd
+
+  download_resources(uris)
+end
+
+
+if __FILE__ == $0
+  usage = <<-EOU
+
+usage: ruby #{File.basename($0)} sources.txt
+
+  The file sources.txt should contain an URL and the target file name.
+  The expected file format is:
+
+  http://www.domain.com/file target_file_name
+  ftp://www.domain.com/file target_file_name
+
+  EOU
+
+  abort usage if ARGV.length != 1
+
+  main
+
+end
