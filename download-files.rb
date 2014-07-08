7318fcefe94ab03c625f962ffb1b56b1d4959982
Marcos Serpa <marcosserpa@gmail.com>
2014-07-08 12:58:10 -0300

    Changes some formatation and better reading.

--- download-files.rb
+++ download-files.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 
 # Ruby script to download .wav files from individual URLs via HTTP/HTTPS/FTP specified in an external file.
-# Author: Marcos Serpa (some source from Tobias Preuss)
+# Author: Marcos Serpa (some code from Tobias Preuss)
 # License: Creative Commons Attribution-ShareAlike 3.0 Unported
 
 require 'net/http'
@@ -44,38 +44,48 @@ end
 
 def read_uris_from_file(file)
   uris = Array.new
+
   File.open(file).each do |line|
     line = line.strip
+
     next if line == nil || line.length == 0
+
     parts = line.split(' ')
     pair = Hash[ [:resource, :filename].zip(parts) ]
     uris.push(pair)
   end
+
   uris
 end
 
+# Decides the download method by the protocol
 def download_resource(resource, filename)
   uri = URI.parse(resource)
+
   case uri.scheme.downcase
   when /http|https/
     http_download_uri(uri, filename)
   when /ftp/
     ftp_download_uri(uri, filename)
   else
-    puts "Unsupported URI scheme for resource " + resource + "."
+    puts "Unsupported URI protocol in " + resource + "."
   end
 end
 
 def http_download_uri(uri, filename)
   puts "Starting HTTP download for: " + uri.to_s
+
   http_object = Net::HTTP.new(uri.host, uri.port)
   http_object.use_ssl = true if uri.scheme == 'https'
+
   begin
     http_object.start do |http|
       request = Net::HTTP::Get.new uri.request_uri
+
       http.read_timeout = 500
-      http.request request do |response|
-        open filename, 'w' do |io|
+
+      http.request(request) do |response|
+        open(filename, 'w') do |io|
           response.read_body do |chunk|
             io.write chunk
           end
@@ -84,15 +94,19 @@ def http_download_uri(uri, filename)
     end
   rescue Exception => e
     puts "=> Exception: '#{e}'. Skipping download."
+
     return
   end
+
   puts "Stored download as " + filename + "."
 end
 
 def ftp_download_uri(uri, filename)
   puts "Starting FTP download for: " + uri.to_s + "."
+
   dirname = File.dirname(uri.path)
   basename = File.basename(uri.path)
+
   begin
     Net::FTP.open(uri.host) do |ftp|
       ftp.login
@@ -101,8 +115,10 @@ def ftp_download_uri(uri, filename)
     end
   rescue Exception => e
     puts "=> Exception: '#{e}'. Skipping download."
+
     return
   end
+
   puts "Stored download as " + filename + "."
 end
 
@@ -110,6 +126,7 @@ def download_resources(pairs)
   pairs.each do |pair|
     filename = pair[:filename].to_s
     resource = pair[:resource].to_s
+
     unless File.exists?(filename)
       download_resource(resource, filename)
     else
@@ -147,18 +164,17 @@ end
 if __FILE__ == $0
   usage = <<-EOU
 
-usage: ruby #{File.basename($0)} sources.txt
+  usage: type 'ruby #{File.basename($0)} sources.txt' where 'sources.txt' is the file with 
+    de URIs of the files to be accessed and downloaded
 
-  The file sources.txt should contain an URL and the target file name.
-  The expected file format is:
+    The file sources.txt should contain at least an URL and the target file name. Like this:
 
-  http://www.domain.com/file target_file_name
-  ftp://www.domain.com/file target_file_name
+    http://www.domain.com/file target_file_name
+    ftp://www.domain.com/file target_file_name
 
-  EOU
+    EOU
 
   abort usage if ARGV.length != 1
 
   main
-
 end
